f55565f5c75f0f49cb7f54ff59f03baae19f32ba
Jabulani Sifundza <jjsifundza@gmail.com>
2022-09-28 11:37:26 +0200

    Module Bug fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+import * as tf from '@tensorflow/tfjs';
+import * as speech from '@tensorflow-models/speech-commands';
+
+const App = () => {
+	const [model, setModel] = useState(null);
+	const [action, setAction] = useState(null);
+	const [labels, setLabels] = useState(null);
+
+	//Recognizer
+	const loadModel = async ()=>{
+		const recognizer = await speech.create("BROWSER_FFT");
+		console.log("Model has been loaded");
+		await recognizer.ensureModelLoaded();
+		console.log(recognizer.wordLabels());
+		setModel(recognizer);
+		setLabels(recognizer.wordLabels());
+	}
+
+	useEffect(()=>{
+		loadModel()
+	},[]);
+
+
+	function argMax(arr){
+		return arr.map((x,i)=>[x, i]).reduce((r,a)=>(a[0] < r[0] ? a:r))[1];
+	}
+
+
+
+	const recognizeCommands = async ()=>{
+		console.log("Listening for commands");
+		model.listen(result =>{
+			console.log(result);
+			setAction(labels[argMax(Object.values(result.scores))]);
+			console.log(action);
+		},{includeSpectrogram: true, probabilityThreshold: 0.7})
+		setTimeout(()=> model.stopListening(), 10e3)
+	}
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<button onClick={recognizeCommands}>Command</button>
+	{
+		action ? <div>{action}</div> : <div>No command detected</div>
+	}
+      
     </div>
   );
 }
